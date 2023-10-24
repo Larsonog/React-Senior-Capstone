283fd9cc8f50f8ba3bf98ae46e374eaed5863df7
Olivia <larsonog@hendrix.edu>
2023-10-23 22:49:31 -0500

    starting clickable list on entertainment page

--- vite-tw/src/pages/4.entertainment/Entertainment.jsx
+++ vite-tw/src/pages/4.entertainment/Entertainment.jsx
@@ -9,7 +9,8 @@ const posts = [
     datetime: '',
     category: { title: 'Outdoor', href: '#' },
     author: {}, // if I remove this, it gets angry
-    image:  './assets/Cadron.jpeg',
+    image: './assets/Cadron.jpeg',
+    list: "Cadron Ridge, Beaver Fork, Hendrix Creek Preserve, etc"
   },
 
   {
@@ -21,8 +22,9 @@ const posts = [
     date: '',
     datetime: '',
     category: { title: 'Group Activites', href: '#' },
-    author: {}, 
-    image:  './assets/rec.jpeg',
+    author: {},
+    image: './assets/rec.jpeg',
+    list: "Adult Volleyball league, Bowling rec league, Chess club, etc"
   },
   {
     id: 3,
@@ -33,10 +35,11 @@ const posts = [
     date: '',
     datetime: '',
     category: { title: 'Miscelaneous', href: '#' },
-    author: {}, 
-    image:  './assets/downtown.jpeg',
+    author: {},
+    image: './assets/downtown.jpeg',
+    list: "grab a drink at Kings, check out Faulkner County public library,etc"
   },
-  
+
   // More posts...
 ]
 
@@ -50,47 +53,45 @@ export default function Entertainment() {
             Stuck on what you should do? Here are some fun activies you can do in Conway!.
           </p>
         </div>
-        <div className="mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 border-t border-gray-200 pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-3">
-          {posts.map((post) => (
-            
-            <article key={post.id} className="flex max-w-xl flex-col items-start justify-between">
-              <div>
-                <img src={post.image} alt="" className="h-100 w-100 rounded bg-gray-50" style={{height:250, width:400}} />
-              </div>
-              <div className="flex items-center gap-x-4 text-xs">
-                <time dateTime={post.datetime} className="text-gray-500">
-                  {post.date}
-                </time>
-                <a
-                  href={post.category.href}
-                  className="relative z-10 rounded-full bg-gray-50 px-3 py-1.5 font-medium text-gray-600 hover:bg-gray-100"
-                >
-                  {post.category.title}
-                </a>
-              </div>
-              
-              <div className="group relative">
-                <h3 className="mt-3 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600">
-                  <a href={post.href}>
-                    <span className="absolute inset-0" />
-                    {post.title}
-                  </a>
-                </h3>
-                <p className="mt-5 line-clamp-3 text-sm leading-6 text-gray-600">{post.description}</p>
-              </div>
-              <div className="relative mt-8 flex items-center gap-x-4">
-                
-                <div className="text-sm leading-6">
-                  <p className="font-semibold text-gray-900">
+        <button onClick={() => alert("{post.list}")}>
+          <div className="mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 border-t border-gray-200 pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-3">
+            {posts.map((post) => (
 
-                  </p>
-                  <p className="text-gray-600">{post.author.role}</p>
+              <article key={post.id} className="flex max-w-xl flex-col items-start justify-between">
+                <div>
+                  <img src={post.image} alt="" className="h-100 w-100 rounded bg-gray-50" style={{ height: 250, width: 400 }} />
                 </div>
-              </div>
-            </article>
-          ))}
-        </div>
+                <div className="flex items-center gap-x-4 text-xs">
+                  <time dateTime={post.datetime} className="text-gray-500">
+                    {post.date}
+                  </time>
+                  <a
+                    href={post.category.href}
+                    className="relative z-10 rounded-full bg-gray-50 px-3 py-1.5 font-medium text-gray-600 hover:bg-gray-100"
+                  >
+                    {post.category.title}
+                  </a>
+                </div>
+
+                <div className="group relative">
+                  <h3 className="mt-3 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600">
+                    <a href={post.href}>
+                      <span className="absolute inset-0" />
+                      {post.title}
+                    </a>
+                  </h3>
+                  <p className="mt-5 line-clamp-3 text-sm leading-6 text-gray-600">{post.description}</p>
+                </div>
+
+              </article>
+            ))}
+          </div>
+        </button>
       </div>
     </div>
   )
 }
+
+
+//https://www.npmjs.com/package/react-clickable 
+// to make the posts clickable into lists 
