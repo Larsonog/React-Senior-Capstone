ee88b01241f47c729da49e3a4739acd0fbbad6ed
Olivia <larsonog@hendrix.edu>
2023-11-14 10:44:40 -0600

    Working on Editing reactCalendar for events page
    there are so many dependency issues between the different open source libraries that I don't think i can use as many as I wanted

--- vite-tw/src/pages/2.events/Events.jsx
+++ vite-tw/src/pages/2.events/Events.jsx
@@ -4,13 +4,15 @@ import 'react-calendar/dist/Calendar.css';
 import { useState } from 'react';
 import Calendar1 from "./Calendar";
 //import App from "./Calendar2";
+//import apiCalendar from "./GoogleCalendar"
+import Calendar3 from './NewCalendar';
 
 const Events = () => {
   const [date, setDate] = useState(new Date())
   return (
     <div>
       <h1>Events</h1>
-      <Calendar1/>
+      <Calendar3/>
     </div>
   )
 }
